96b32c7579defc92ad844c18a5065e591856429f
The-End-Hero <527409987@qq.com>
2018-10-25 00:32:04 +0800

    merge sort

--- src/JavaScript/数据结构与算法/test-javascript/冒泡_插入_选择排序.js
+++ src/JavaScript/数据结构与算法/test-javascript/冒泡_插入_选择排序.js
@@ -21,7 +21,7 @@ const bubbleSort = (arr) => {
 const insertionSort = (arr) => {
     if (arr.length <= 1) return
     for (let i = 1; i < arr.length; i++) {
-        const temp = arr[i]
+        const temp = arr[i] // 1
         let j = i - 1
         // 若arr[i]前有大于arr[i]的值的化，向后移位，腾出空间，直到一个<=arr[i]的值
         for (j; j >= 0; j--) {
